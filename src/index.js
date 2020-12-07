26b82eebf4d9113f96a3b70f883ba4db73831195
Józsa Ernő <jerno@me.com>
2020-12-07 15:27:32 +0100

    :chart_with_upwards_trend: Remove metric generation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle'
 import App from './app/App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,5 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals(console.debug);
